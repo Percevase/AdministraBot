285a35e7e73a107c580140aaa980e794da8d6fdf
barthelemy drabczuk <barth.drabczuk@gmail.com>
2018-03-17 15:49:07 +0100

    beginning of the bot core functions

--- core.py
+++ core.py
@@ -1,2 +1,11 @@
 import discord
 from discord.ext import commands
+
+bot = commands.Bot(command_prefix='#', description="type #help")
+
+@bot.event
+async def on_ready():
+    print (bot.user.name)
+	print ("has come to kick your ass !")
+	
+@bot.command()
