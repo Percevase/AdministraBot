3c6593361002df47276e3077db0ecc7a6a68a9cb
Bathelemy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-05-04 21:10:04 +0200

    ajout d'un espace

--- core.py
+++ core.py
@@ -6,6 +6,6 @@ bot = commands.Bot(command_prefix='#', description="type #help")
 @bot.event
 async def on_ready():
     print (bot.user.name)
-	print ("has come to kick your ass !")
+	print (" has come to kick your ass !")
 	
 @bot.command()
